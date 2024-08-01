b95360f66760c3474d708acbf79ea25620b57ce8
justingreco <justin.greco@gmail.com>
2024-08-01 10:19:50 -0400

    changed base for github

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/raleighwater/hazard-form/',
+  base: '/hazard-form/',
   plugins: [react()],
 })
 
